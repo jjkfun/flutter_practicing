edc6263d69bd4fe2fbf5556d44c593751daeb04e
jjkfun <sukunaryomen782@gmail.com>
2025-01-13 19:29:25 +0600

    надеюсь это ружьё

--- lib/2.dart
+++ lib/2.dart
@@ -8,119 +8,125 @@ class SecondScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        leading: IconButton(
-          onPressed: () {
-            Navigator.pop(context);
-          },
-          icon: const Icon(Icons.arrow_back_ios),
-          color: Colors.black,
-        ),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.only(left: 14, right: 14, top: 10),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                const Text(
-                  'Today',
-                  style: TextStyle(
-                    fontSize: 30,
-                    fontWeight: FontWeight.w700,
-                  ),
-                ),
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.teal,
-                  ),
-                  onPressed: () {
-                    showDialog(
-                      context: context,
-                      builder: (context) {
-                        String title = '';
-                        String subtitle = '';
-                        return AlertDialog(
-                          title: const Text('Add Task'),
-                          content: Column(
-                            mainAxisSize: MainAxisSize.min,
-                            children: [
-                              TextField(
-                                decoration:
-                                    const InputDecoration(labelText: 'Title'),
-                                onChanged: (value) => title = value,
-                              ),
-                              TextField(
-                                decoration: const InputDecoration(
-                                    labelText: 'Subtitle'),
-                                onChanged: (value) => subtitle = value,
-                              ),
-                            ],
-                          ),
-                          actions: [
-                            TextButton(
-                              onPressed: () {
-                                Navigator.pop(context);
-                              },
-                              child: const Text('Cancel'),
-                            ),
-                            TextButton(
-                              onPressed: () {
-                                context.read<TodoBloc>().add(
-                                      AddTaskEvent(
-                                        newTask: TodoModel(
-                                          title: title,
-                                          subtitle: subtitle,
-                                        ),
-                                      ),
-                                    );
-                                Navigator.pop(context);
-                              },
-                              child: const Text('Add'),
-                            ),
-                          ],
-                        );
-                      },
-                    );
-                  },
-                  child: const Text(
-                    'Add task',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 16,
-                      fontWeight: FontWeight.w700,
-                    ),
-                  ),
-                ),
-              ],
+    return BlocBuilder<TodoBloc, TodoState>(
+      builder: (context, state) {
+        if (state is TodoInitial) {
+          return const Scaffold(
+            body: Center(
+              child: CircularProgressIndicator(),
             ),
-            const Text(
-              'Productive Day, Sourav',
-              style: TextStyle(
-                fontSize: 22,
-                fontWeight: FontWeight.w500,
-                color: Colors.black26,
+          );
+        } else if (state is TodoLoaded) {
+          final todos = state.todos;
+          return Scaffold(
+            appBar: AppBar(
+              leading: IconButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                icon: const Icon(Icons.arrow_back_ios),
+                color: Colors.black,
               ),
             ),
-            const SizedBox(height: 18),
-            Expanded(
-              child: BlocBuilder<TodoBloc, TodoState>(
-                builder: (context, state) {
-                  if (state is TodoLoaded) {
-                    // return const Center(child: Text('Ошибка состояния')); // раскоментируйте
-                    return ListView.builder(
-                      itemCount: state.todos.length,
+            body: Padding(
+              padding: const EdgeInsets.only(left: 14, right: 14, top: 10),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      const Text(
+                        'Today',
+                        style: TextStyle(
+                          fontSize: 30,
+                          fontWeight: FontWeight.w700,
+                        ),
+                      ),
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.teal,
+                        ),
+                        onPressed: () {
+                          showDialog(
+                            context: context,
+                            builder: (context) {
+                              String title = '';
+                              String subtitle = '';
+                              return AlertDialog(
+                                title: const Text('Add Task'),
+                                content: Column(
+                                  mainAxisSize: MainAxisSize.min,
+                                  children: [
+                                    TextField(
+                                      decoration: const InputDecoration(
+                                          labelText: 'Title'),
+                                      onChanged: (value) => title = value,
+                                    ),
+                                    TextField(
+                                      decoration: const InputDecoration(
+                                          labelText: 'Subtitle'),
+                                      onChanged: (value) => subtitle = value,
+                                    ),
+                                  ],
+                                ),
+                                actions: [
+                                  TextButton(
+                                    onPressed: () {
+                                      Navigator.pop(context);
+                                    },
+                                    child: const Text('Cancel'),
+                                  ),
+                                  TextButton(
+                                    onPressed: () {
+                                      context.read<TodoBloc>().add(
+                                            AddTaskEvent(
+                                              newTask: TodoModel(
+                                                title: title,
+                                                subtitle: subtitle,
+                                              ),
+                                            ),
+                                          );
+                                      Navigator.pop(context);
+                                    },
+                                    child: const Text('Add'),
+                                  ),
+                                ],
+                              );
+                            },
+                          );
+                        },
+                        child: const Text(
+                          'Add task',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 16,
+                            fontWeight: FontWeight.w700,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  const Text(
+                    'Productive Day, Sourav',
+                    style: TextStyle(
+                      fontSize: 22,
+                      fontWeight: FontWeight.w500,
+                      color: Colors.black26,
+                    ),
+                  ),
+                  const SizedBox(height: 18),
+                  Expanded(
+                    child: ListView.builder(
+                      itemCount: todos.length,
                       itemBuilder: (context, index) {
                         return ListTile(
                           leading: const CircleAvatar(
                             backgroundColor: Colors.indigoAccent,
                           ),
-                          title: Text(state.todos[index].title),
+                          title: Text(todos[index].title),
                           subtitle: Text(
-                            state.todos[index].subtitle,
+                            todos[index].subtitle,
                             style: const TextStyle(fontSize: 13),
                           ),
                           trailing: Row(
@@ -131,10 +137,9 @@ class SecondScreen extends StatelessWidget {
                                   showDialog(
                                     context: context,
                                     builder: (context) {
-                                      String updatedTitle =
-                                          state.todos[index].title;
+                                      String updatedTitle = todos[index].title;
                                       String updatedSubtitle =
-                                          state.todos[index].subtitle;
+                                          todos[index].subtitle;
                                       return AlertDialog(
                                         title: const Text('Update Task'),
                                         content: Column(
@@ -186,32 +191,59 @@ class SecondScreen extends StatelessWidget {
                                     },
                                   );
                                 },
-                                icon: const Icon(Icons.edit),
+                                icon: Icon(todos[index].data1),
                               ),
                               IconButton(
                                 onPressed: () {
-                                  context
-                                      .read<TodoBloc>()
-                                      .add(DeleteTaskEvent(index: index));
+                                  showDialog(
+                                    context: context,
+                                    builder: (context) {
+                                      return AlertDialog(
+                                        title: const Text(
+                                          'Вы точно хотите удалить таску?',
+                                        ),
+                                        actions: [
+                                          TextButton(
+                                            onPressed: () {
+                                              Navigator.pop(context);
+                                            },
+                                            child: const Text('Cancel'),
+                                          ),
+                                          TextButton(
+                                            onPressed: () {
+                                              context.read<TodoBloc>().add(
+                                                    DeleteTaskEvent(
+                                                        index: index),
+                                                  );
+                                              Navigator.pop(context);
+                                            },
+                                            child: const Text('Delete'),
+                                          ),
+                                        ],
+                                      );
+                                    },
+                                  );
                                 },
-                                icon: const Icon(Icons.delete),
+                                icon: Icon(todos[index].data2),
                               ),
                             ],
                           ),
                         );
                       },
-                    );
-                  } else if (state is TodoInitial) {
-                    context.read<TodoBloc>().add(LoadTodosEvent(todos: todos));
-                    return const Center(child: CircularProgressIndicator());
-                  }
-                  return const Center(child: Text('No tasks available.'));
-                },
+                    ),
+                  ),
+                ],
               ),
             ),
-          ],
-        ),
-      ),
+          );
+        } else {
+          return const Scaffold(
+            body: Center(
+              child: Text('Произошла ошибка. Попробуйте позже.'),
+            ),
+          );
+        }
+      },
     );
   }
 }
